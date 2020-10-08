5e0924e00d9c8e85026d689d69a0362219fb205e
Kali Correll <kalicorrell18@gmail.com>
2020-10-08 16:03:35 -0400

    Ch13 NameParserApp

--- ch13_ex1_NameParser/src/NameParserApp.java
+++ ch13_ex1_NameParser/src/NameParserApp.java
@@ -11,6 +11,42 @@ public class NameParserApp {
         System.out.print("Enter a name: ");
         String name = sc.nextLine();
         System.out.println();
+        name = name.trim();
         
+        int index1 = name.indexOf(" ");
+      if (index1 == -1) {
+    	  System.out.println("Name not in valid format. Please try again");
+      }
+      else {
+    	  int index2 = name.indexOf(" ",index1+1);
+    	  if (index2 == -1) {
+    		  String firstName = name.substring(0, index1);
+    		  String lastName = name.substring(index1+1);
+    		  System.out.println("First Name: "+ firstName);
+    		  System.out.println("Last Name: "+ lastName);
+    	  }
+    	  else {
+    		  int index3 = name.indexOf(" ",index2+1);
+    		 if (index3 == -1) {
+    			 String firstName = name.substring(0, index1);
+       		  String middleName = name.substring(index1 + 1,index2);
+    			 String lastName = name.substring(index2+1);
+    		
+    		 System.out.println("First Name:   "+ firstName);
+    		 System.out.println("Middle Name:   "+ middleName);
+    		 System.out.println("Last Name:   "+ lastName);
+    		 
+    		 }
+    		 else {
+    			 System.out.println("Name not in valid format.");
+    			 
+    		 }
+    		  
+    	  
+    	  }
+      
+      
+      }
+    
     }
 }
