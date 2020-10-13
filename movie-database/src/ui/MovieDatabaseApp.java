4334efd506e16569ddf9ccabdfc4e0634801d4b1
Kali Correll <kalicorrell18@gmail.com>
2020-10-12 22:44:52 -0400

    Update to Movie Database App

--- movie-database/src/ui/MovieDatabaseApp.java
+++ movie-database/src/ui/MovieDatabaseApp.java
@@ -18,12 +18,13 @@ public class MovieDatabaseApp {
 
 	public static void main(String[] args) {
 
-		//movies.add(new Movie(1, "Star Wars", "Sci-fi", "1977", "PG"));
-		//movies.add(new Movie(2, "Star Trek", "Sci-fi", "1980", "G"));
-	///	movies.add(new Movie(3, "Gone With The Wind", "Musical", "1960", "G")); // java needs to be told your adding
-																				// something new
+		// movies.add(new Movie(1, "Star Wars", "Sci-fi", "1977", "PG"));
+		// movies.add(new Movie(2, "Star Trek", "Sci-fi", "1980", "G"));
+		/// movies.add(new Movie(3, "Gone With The Wind", "Musical", "1960", "G")); //
+		/// java needs to be told your adding
+		// something new
 
-System.out.println("Welcome to the Movie Database App! ");
+		System.out.println("Welcome to the Movie Database App! ");
 		int command = 0;
 
 		while (command != 7) {
@@ -43,14 +44,12 @@ System.out.println("Welcome to the Movie Database App! ");
 				String bd = Console.getString("Birth Date?: ");
 				LocalDate ld = LocalDate.parse(bd);
 				Actor actor = new Actor(id, fn, ln, g, ld);
-				if(actorDAO.add(actor)) {
-					 System.out.println("Actor added successfully!");
-					}
-					else {
-						System.out.println("Issue adding actor.");
-					}
-					System.out.println();
-				
+				if (actorDAO.add(actor)) {
+					System.out.println("Actor added successfully!");
+				} else {
+					System.out.println("Issue adding actor.");
+				}
+				System.out.println();
 
 				System.out.println("Actor Added!");
 				System.out.println(actor.displayActor());
@@ -71,14 +70,12 @@ System.out.println("Welcome to the Movie Database App! ");
 			case 3:
 				// find Actor
 				System.out.println("*** Not yet implemented**");
-				//id = Console.getInt("Actor ID?");
-				//for (Actor a : actors) {
-				//	if (a != null && a.getActorID() == id) {
-					//	System.out.println("Actor Found!");
-					//	System.out.println(a.displayActor());
-					//	System.out.println();
-					
-				
+				// id = Console.getInt("Actor ID?");
+				// for (Actor a : actors) {
+				// if (a != null && a.getActorID() == id) {
+				// System.out.println("Actor Found!");
+				// System.out.println(a.displayActor());
+				// System.out.println();
 
 				break;
 
@@ -92,21 +89,18 @@ System.out.println("Welcome to the Movie Database App! ");
 				String gr = Console.getLine("Genre? ");
 
 				Movie movie = new Movie(i, t, y, r, gr);
-				if(movieDAO.add(movie)) {
-					 System.out.println("Movie added successfully!");
-					}
-					else {
-						System.out.println("Issue adding movie.");
-					}
-					System.out.println();
-				
+				if (movieDAO.add(movie)) {
+					System.out.println("Movie added successfully!");
+				} else {
+					System.out.println("Issue adding movie.");
+				}
+				System.out.println();
 
 				System.out.println("Movie Added!");
 				System.out.println(movie.displayMovie());
 				System.out.println();
 				break;
 
-				
 			case 5:
 				// list Movies
 				System.out.println("List of all Movies");
@@ -122,45 +116,36 @@ System.out.println("Welcome to the Movie Database App! ");
 			case 6:
 				// find Movie
 				System.out.println("**Not implemented yet**");
-				//id = Console.getInt("Movie ID?");
-				//for (Movie m : movies) {
-					//if (m != null && m.getMovieID() == id) {
-						//System.out.println("Movie Found!");
-						//System.out.println(m.displayMovie());
-				
+				// id = Console.getInt("Movie ID?");
+				// for (Movie m : movies) {
+				// if (m != null && m.getMovieID() == id) {
+				// System.out.println("Movie Found!");
+				// System.out.println(m.displayMovie());
 
 				System.out.println();
 				break;
-				
+
 			case 7:
-			// delete an actor
-				 
-				System.out.println("Delete an actor");
+				// delete an actor
 				// prompt user for id to delete use console class
 				// get the actor for that id / actorDAO.get
-			    // delete the actor /actorDAO.delete
-			
-			   actor = actorDAO.get(id); {
-			   if(actorDAO != null) {
-				   boolean success = actorDAO.delete(actor);
-		            if (success) {
-		                System.out.println(actor.displayActor()
-		                        + " has been deleted from the database.\n");
-		            } 
-		            else {
-		                System.out.println("Error! Unable to delete actor.\n");
-		            }
-		        } 
-			   else {
-		            System.out.println("No product matches that code.\n");
-		            break;
-		        }
-			   }
-				
-				
-				
-				
-				
+				// delete the actor /actorDAO.delete
+				System.out.println("Delete an actor");
+		        int actorID = Console.getInt("Enter Movie ID to delete it: ");
+				Actor a  = actorDAO.get(actorID); {
+				if (actorDAO != null) {
+					boolean success = actorDAO.delete(actorID);
+					if (success) {
+						System.out.println(actor.displayActor() + " has been deleted from the database.\n");
+					} else {
+						System.out.println("Error! Unable to delete actor.\n");
+					}
+				} else {
+					System.out.println("No product matches that code.\n");
+					break;
+				}
+			}
+
 			case 8:
 				// exit
 				break;
